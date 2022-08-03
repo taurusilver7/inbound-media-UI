c1736e87c713e71183e430e05aa960e76919f02c
taurusilver7 <prasad20697@gmail.com>
2022-08-04 02:44:05 +0530

    profile added with upload

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,14 +2,21 @@ import React from "react";
 import Card from "../card/Card";
 import Followers from "../followers/Followers";
 import Search from "../search/Search";
-import "./Profile.css"
+import "./Profile.css";
 
 const Profile = () => {
-  return <div className="profile">
-    <Search />
-    <Card />
-    <Followers />
-  </div>;
+  return (
+    <div className="profile">
+      {/* Top : logo & searchbar */}
+      <Search />
+
+      {/* User profile Card: img, details, followers count */}
+      <Card />
+
+      {/* followers + details */}
+      <Followers />
+    </div>
+  );
 };
 
 export default Profile;
