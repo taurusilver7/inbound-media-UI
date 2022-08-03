b921ffbbe20628de146eae032ee91727153a0f09
taurusilver7 <prasad20697@gmail.com>
2022-08-04 00:57:22 +0530

    card added to profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Card from "../card/Card";
 import Search from "../search/Search";
 import "./Profile.css"
 
 const Profile = () => {
   return <div className="profile">
     <Search />
+    <Card />
   </div>;
 };
 
