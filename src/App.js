99d63a3abc9cbc4189c1722f4539f855b987c06c
taurusilver7 <prasad20697@gmail.com>
2022-08-04 00:20:41 +0530

    app files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-
+import Home from "./pages/home/Home";
 const App = () => {
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      Inbound Media Build
+      <Home />
     </div>
   );
 };
