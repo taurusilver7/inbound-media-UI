b4819ebfbcac1bb93783450cb76f618c78fb76be
taurusilver7 <prasad20697@gmail.com>
2022-08-04 00:19:33 +0530

    profile component on home page

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "../search/Search";
+import "./Profile.css"
+
+const Profile = () => {
+  return <div className="profile">
+    <Search />
+  </div>;
+};
+
+export default Profile;
