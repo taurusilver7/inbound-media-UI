3da432cbc80012421a4fc86c648a547132614444
taurusilver7 <prasad20697@gmail.com>
2022-08-04 02:44:46 +0530

    posts added to home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Profile from '../../components/profile/Profile'
-import "./Home.css"
+import React from "react";
+import Posts from "../../components/posts/Posts";
+import Profile from "../../components/profile/Profile";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="profile"><Profile /></div>
-      <div className="post">Post</div>
+      <div className="profile">
+        <Profile />
+      </div>
+      <div className="post">
+        <Posts />
+      </div>
       <div className="right">Right</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
