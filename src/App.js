32fcb911b0cdd3c16472c0c55a846d3ca97f06c7
taurusilver7 <prasad20697@gmail.com>
2022-08-03 23:30:16 +0530

    main app file created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      Inbound Media Build
+    </div>
+  );
+};
+
+export default App;
