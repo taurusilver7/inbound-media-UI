258a50f8f8c51d167ca39d135474c7ca3f704eb0
taurusilver7 <prasad20697@gmail.com>
2022-08-04 12:32:29 +0530

    mock post data

---
+++ src/Data/PostData.js
@@ -0,0 +1,27 @@
+import postPic1 from "../img/postpic1.jpg";
+import postPic2 from "../img/postpic2.jpg";
+import postPic3 from "../img/postpic3.JPG";
+
+export const PostData = [
+  {
+    img: postPic1,
+    name: "Marika Tachibana",
+    desc: "Happy new Year",
+    likes: 2300,
+    liked: true,
+  },
+  {
+    img: postPic2,
+    name: "Eren Yeager",
+    desc: "Party time:3",
+    likes: 2300,
+    liked: false,
+  },
+  {
+    img: postPic3,
+    name: "Percy Jackson",
+    desc: "Happy new Year",
+    likes: 2300,
+    liked: true,
+  },
+];
