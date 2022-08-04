ca9be61f7b6517f4e28f10a4222e6f385b874e69
taurusilver7 <prasad20697@gmail.com>
2022-08-04 12:32:52 +0530

    mock trends data

---
+++ src/Data/TrendData.js
@@ -0,0 +1,14 @@
+export const TrendData = [
+  {
+    name: "Avengers",
+    shares: 80.5,
+  },
+  {
+    name: "Marvels",
+    shares: 60,
+  },
+  {
+    name: "Eternals",
+    shares: 75.2,
+  },
+];
