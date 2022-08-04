a49370bce39504be994e7bd26f567623f5383ae0
taurusilver7 <prasad20697@gmail.com>
2022-08-04 18:14:58 +0530

    suggestions share modal

---
+++ src/components/shareModal/ShareModal.jsx
@@ -0,0 +1,18 @@
+import { Modal } from "@mantine/core";
+import Upload from "../upload/Upload";
+
+const ShareModal = ({ opened, setOpened }) => {
+  return (
+    <Modal
+      opened={opened}
+      onClose={() => setOpened(false)}
+      title="Your Trends"
+      overlayBlur={3}
+      size="55%"
+    >
+      <Upload />
+    </Modal>
+  );
+};
+
+export default ShareModal;
