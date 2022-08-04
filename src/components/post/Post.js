e9d49412d55a71c7e5607f6a386815861314958d
taurusilver7 <prasad20697@gmail.com>
2022-08-04 12:34:31 +0530

    ind post component in posts

---
+++ src/components/post/Post.js
@@ -0,0 +1,34 @@
+// induvidual post component cards in Result wrapper component. The Result is embedded into the Post component (The middle grid.)
+
+import React from "react";
+import "./Post.css";
+
+import Comment from "../../img/comment.png";
+import NotLike from "../../img/notlike.png";
+import Like from "../../img/like.png";
+import Share from "../../img/share.png";
+
+const Post = ({ post }) => {
+  return (
+    <div className="post">
+      <img className="postImg" src={post.img} alt={post.name} />
+
+      <div className="postButtons">
+        <img src={post.liked ? Like : NotLike} alt="like_button" />
+        <img src={Comment} alt="comment_button" />
+        <img src={Share} alt="share_button" />
+      </div>
+
+      <span>{post.likes} likes</span>
+
+      <div className="detail">
+        <span>
+          <b>{post.name}</b>
+        </span>
+        <span>{post.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
