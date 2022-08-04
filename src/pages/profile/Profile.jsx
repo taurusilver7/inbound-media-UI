7e883d928c96b5a0aab82302e13009ebcf505b9c
taurusilver7 <prasad20697@gmail.com>
2022-08-04 13:46:24 +0530

    profile page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "../../components/card/Card";
+import Followers from "../../components/followers/Followers";
+import InfoCard from "../../components/infoCard/InfoCard";
+import Results from "../../components/results/Results";
+import Search from "../../components/search/Search";
+import Suggestions from "../../components/suggestions/Suggestions";
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="left">
+        <Search />
+        <InfoCard />
+        <Followers />
+      </div>
+      <div className="center">
+        <Card />
+        <Results />
+      </div>
+      <div className="right">
+        <Suggestions />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
