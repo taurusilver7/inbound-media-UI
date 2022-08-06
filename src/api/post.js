d1731fb496ff3761555ee464c68d3e3e9cf7ae80
taurusilver7 <prasad20697@gmail.com>
2022-08-07 00:01:21 +0530

    like posts endpoint

--- src/api/post.js
+++ src/api/post.js
@@ -5,3 +5,5 @@ const API = axios.create({ baseURL: "http://localhost:5000/api/v1" });
 export const createPost = (data) => API.post("/post", data);
 
 export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
+
+export const likePost = (id, userId) => API.put(`/post/${id}/like`, { userId });
