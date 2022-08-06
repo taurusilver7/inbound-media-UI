1227a0e047e558df70f86423bdcb650c99fb53dd
taurusilver7 <prasad20697@gmail.com>
2022-08-06 22:20:20 +0530

    post reducer states

---
+++ src/reducers/post.js
@@ -0,0 +1,31 @@
+// Almost all actions marked as UPLOAD are applied in Upload component.
+// The actions marked RETREVING belong to posts component.
+
+const postReducer = (
+  state = { posts: [], loading: false, error: false, uploading: false },
+  action
+) => {
+  switch (action.type) {
+    case "UPLOAD_START":
+      return { ...state, uploading: true, error: false };
+    case "UPLOAD_SUCCESS":
+      return {
+        ...state,
+        error: false,
+        uploading: false,
+        posts: [action.data, ...state.posts],
+      };
+    case "UPLOAD_FAIL":
+      return { ...state, error: true, uploading: false };
+    case "RETREIVING_START":
+      return { ...state, loading: true, error: false };
+    case "RETREIVING_SUCCESS":
+      return { ...state, post: action.data, loading: true, error: false };
+    case "RETREIVING_FAIL":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
