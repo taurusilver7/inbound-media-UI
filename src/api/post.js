3e1ab2ebcfa97001df4567867d278744ae6ee336
taurusilver7 <prasad20697@gmail.com>
2022-08-06 22:18:46 +0530

    post api enpoint created

---
+++ src/api/post.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:5000/api/v1" });
+
+export const createPost = (data) => API.post("/post", data);
+
+export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
