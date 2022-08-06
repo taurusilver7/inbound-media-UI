22836ba820e9b078a44640c4bf9e999774e4a3fd
taurusilver7 <prasad20697@gmail.com>
2022-08-06 22:20:52 +0530

    post wrapper component

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,16 +1,28 @@
 // The wrapper component that lies beneath the upload component in results center section (middle grid).
 
-import React from "react";
+import React, { useEffect } from "react";
 import "./Posts.css";
 import { PostData } from "../../data/PostData";
 import Post from "../post/Post";
+import { useDispatch, useSelector } from "react-redux";
+import { getTimelinePosts } from "../../actions/post";
 
 const Posts = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const { posts, loading } = useSelector((state) => state.postReducer);
+
+  console.log(posts);
+
+  useEffect(() => {
+    dispatch(getTimelinePosts(user?._id));
+  }, [dispatch, user]);
+
   return (
     <div className="posts">
-      {PostData.map((post, id) => (
-        <Post post={post} key={id} />
-      ))}
+      {loading
+        ? "Loading.."
+        : posts.map((post, id) => <Post post={post} key={id} />)}
     </div>
   );
 };
